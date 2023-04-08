968a1b468bc2053f9946e2515386c6e8e3527800
Eduardo <edu0alves00@gmail.com>
2023-04-07 22:21:40 -0300

    create /user api route

--- src/pages/api/user.tsx
+++ src/pages/api/user.tsx
@@ -1,8 +1,42 @@
 import { NextApiRequest, NextApiResponse } from 'next';
+import connect from '../../../utils/database';
 
-interface ResponseType {
-    message: string
+interface ErrorResponseType {
+    error: string
 }
-export default (req: NextApiRequest, res: NextApiResponse<ResponseType>) => {
-    res.status(200).json({message: "A api está funfando"})
+
+interface SuccessResponseType {
+    _id: string,
+    name: string,
+    email: string,
+    phone: string,
+    teacher: string
+}
+
+export default async (
+    req: NextApiRequest,
+    res: NextApiResponse<ErrorResponseType | SuccessResponseType>
+): Promise<void> => {
+
+    if (req.method === "POST") {
+        
+        const { name, email, phone, teacher } = req.body;
+        
+        if(!name || !email || !phone || !teacher) {
+            res.status(400).json({error: 'Missing body parameter'});
+            return;
+        }
+        const { db } = await connect();
+        
+        const response = await db.collection('users').insertOne({
+            name, 
+            email,
+            phone,
+            teacher
+        })
+        res.status(200).json(response.ops[0])
+    } else {
+        res.status(400).json({ error: "Wrong method" })
+    }
+
 }
--- utils/database.ts
+++ utils/database.ts
@@ -1,2 +1,18 @@
-import { MongoClient } from 'mongodb';
+import { Db, MongoClient } from 'mongodb';
 
+interface ConnectType {
+    db: Db;
+    client: MongoClient
+}
+
+const client = new MongoClient(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+export default async function connect(): Promise<ConnectType> {
+   if(!client.isConnected()) await client.connect();
+
+   const db = client.db('evecoding');
+   return { db, client }
+}
